ceaa4db757bf4f501238e15d6f4a8ae2507378f2
noorqidam <noorqidam@gmail.com>
2019-09-03 15:06:53 +0700

    Tugas 17 JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas17.js
@@ -0,0 +1,10 @@
+function tugas17(){
+  var huruf = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+  console.log(huruf.toLowerCase())
+  console.log(huruf.toUpperCase())
+
+  var huruf2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+  console.log(huruf2.toLowerCase())
+  console.log(huruf2.toUpperCase())
+}
+tugas17()
